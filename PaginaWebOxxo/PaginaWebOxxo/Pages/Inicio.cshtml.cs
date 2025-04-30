2d973d1b7be8d21257f2747756b23a75cc95a3be
AlexandroAngulo <152337375+AlexandroAngulo@users.noreply.github.com>
2025-04-30 17:57:57 -0600

    Creacion de Nueva pagina llamada inicio, back-end y front-end, ademas de cambios en el diseño de la letra general como el color.

---
+++ PaginaWebOxxo/PaginaWebOxxo/Pages/Inicio.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PaginaWebOxxo.Pages;
+
+public class InicioModel : PageModel
+{
+
+    public void OnGet()
+    {
+
+    }
+}
