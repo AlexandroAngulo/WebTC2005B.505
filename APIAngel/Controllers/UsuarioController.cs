c9830f47b1ae6944b6d14084dbe94d2b4fcbf78e
AlexandroAngulo <152337375+AlexandroAngulo@users.noreply.github.com>
2025-05-01 19:57:31 -0600

    Creacion de de API get para obtener la informacion de un usuario dado el numero de trabajador

---
+++ APIAngel/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace APIAngel.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+
+
+public class UsuarioController : ControllerBase
+{
+    public string ConnectionString = "Server=127.0.0.1;Port=3306;Database=BD.TC2005B.505;Uid=root;password=;";
+
+
+    [HttpGet("GetUsuarioById/{NumEmpleado}")] //La edad es otra variable para utilizar dentro del endPoint.
+
+    public Usuarios Get(int NumEmpleado)
+    {
+        Usuarios miUsuario = new Usuarios();
+        MySqlConnection conexion = new MySqlConnection(ConnectionString);
+
+        conexion.Open();
+            MySqlCommand cmd = new MySqlCommand("select * from usuarios where NumEmpleado = "+ NumEmpleado, conexion);
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    miUsuario.NumEmpleado = Convert.ToInt32(reader["NumEmpleado"]);
+                    miUsuario.Nombre = reader["Nombre"].ToString();
+                    miUsuario.ApellidoP = reader["ApellidoP"].ToString();
+                    miUsuario.ApellidoM = reader["ApellidoM"].ToString();
+                    miUsuario.FechaNacimiento = Convert.ToDateTime(reader["FechaNacimiento"]);
+                    miUsuario.FechaRegistro = Convert.ToDateTime(reader["FechaRegistro"]);
+                    miUsuario.IdGenero = Convert.ToInt32(reader["IdGenero"]);
+                    miUsuario.IdTipoPuesto = Convert.ToInt32(reader["IdTipoPuesto"]);
+                    miUsuario.IdEstatus = Convert.ToInt32(reader["IdEstatus"]);
+                }
+            }
+        conexion.Close();
+        return miUsuario;
+    }
+}
+
