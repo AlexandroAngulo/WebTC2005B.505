712176b2c53ebeabc6d33925898fe014a00d20b5
Ivanlopezs <A01723255@tec.mx>
2025-05-01 18:35:17 -0600

    Pagina de Capacitaciones Completado

--- PaginaWebOxxo/PaginaWebOxxo/Model/DataBaseContext.cs
+++ PaginaWebOxxo/PaginaWebOxxo/Model/DataBaseContext.cs
@@ -39,41 +39,41 @@ namespace PaginaWebOxxo.Model
             return estadisticas;
         }
         
-        public Usuarios ObtenerUsuarioPorEmpleados(int numEmpleado)
-        {
-            Usuarios usuario = null;
 
-            using (MySqlConnection conexion = GetConnection())
-            {
-                conexion.Open();
-                string query = "SELECT * FROM usuarios WHERE NumEmpleado = @NumEmpleado";
-                using (MySqlCommand cmd = new MySqlCommand(query, conexion))
-                {
-                    cmd.Parameters.AddWithValue("@NumEmpleado", numEmpleado);
 
-                    using (var reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            usuario = new Usuarios
-                            {
-                                NumEmpleado = Convert.ToInt32(reader["NumEmpleado"]),
-                                Nombre = reader["Nombre"].ToString(),
-                                ApellidoP = reader["ApellidoP"].ToString(),
-                                ApellidoM = reader["ApellidoM"].ToString(),
-                                FechaNacimiento = Convert.ToDateTime(reader["FechaNacimiento"]),
-                                FechaRegistro = Convert.ToDateTime(reader["FechaRegistro"]),
-                                IdGenero = Convert.ToInt32(reader["IdGenero"]),
-                                IdTipoPuesto = Convert.ToInt32(reader["IdTipoPuesto"]),
-                                IdEstatus = Convert.ToInt32(reader["IdEstatus"])
-                            };
-                        }
-                    }
-                }
-            }
-            return usuario;
-        }
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public List<NivelUsuario> ObtenerProgresoPorEmpleado(int numEmpleado)
         {
             List<NivelUsuario> progresos = new List<NivelUsuario>();
