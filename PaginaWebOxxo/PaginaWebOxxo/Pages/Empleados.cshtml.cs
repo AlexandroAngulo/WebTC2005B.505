ee7515a4b9c9451d60b0b5ae19e14b2b9816d29f
Dario <a00838287@tec.mx>
2025-05-22 16:07:29 -0600

    AgregarEmpleados

---
+++ PaginaWebOxxo/PaginaWebOxxo/Pages/Empleados.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PaginaWebOxxo.Pages;
+
+public class Empleados : PageModel
+{
+
+    public void OnGet()
+    {
+
+    }
+}
