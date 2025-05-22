167fc0bcf318e786e10d8c480d88e06e4947219f
ChanchayMateo <a00840391@tec.mx>
2025-05-22 16:23:37 -0600

    Agregarfuncionalidad

--- PaginaWebOxxo/PaginaWebOxxo/Pages/Lider.cshtml.cs
+++ PaginaWebOxxo/PaginaWebOxxo/Pages/Lider.cshtml.cs
@@ -1,13 +1,29 @@
+using PaginaWebOxxo.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace PaginaWebOxxo.Pages;
 
 public class LiderModel : PageModel
 {
 
-    public void OnGet()
-    {
+     private readonly DataBaseContext _context;
+        public LiderModel(DataBaseContext context)
+        {
+            _context = context;
+        }
+    
+    public Estadisticas EstadisticasUsuario { get; set; }
 
-    }
+    public Usuarios Usuario { get; set; }
+    public void OnGet()
+        {
+            int numEmpleado = 12345; 
+            EstadisticasUsuario = _context.ObtenerMonedasPorEmpleado(numEmpleado);
+            Usuario = _context.ObtenerUsuarioPorEmpleados(numEmpleado);
+            
+
+        }
+
 }
