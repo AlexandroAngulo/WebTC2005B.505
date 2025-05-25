58401356a3092518c012644f96847609da564b55
ChanchayMateo <a00840391@tec.mx>
2025-05-25 03:04:39 -0600

    funcionalidad de telefono actualizado

--- PaginaWebOxxo/PaginaWebOxxo/Pages/Lider.cshtml.cs
+++ PaginaWebOxxo/PaginaWebOxxo/Pages/Lider.cshtml.cs
@@ -14,12 +14,17 @@ public class LiderModel : PageModel
         _context = context;
     }
 
-
+    [BindProperty]
     public Estadisticas EstadisticasUsuario { get; set; }
 
+    [BindProperty]
+    public int Telefono { get; set; }
+    [BindProperty]
     public Usuarios Usuario { get; set; }
-
+    [BindProperty]
     public Contacto contacto { get; set; }
+
+    [BindProperty]
     public Codigopostal codigopostal { get; set; }
 
 
@@ -41,7 +46,31 @@ public class LiderModel : PageModel
 
     }
 
+    public void OnPostActualizar()
+    {
+        int numEmpleado = 12345;
+        int codigoPostal = 12345;
 
+        
+
+        string telefonostr = Telefono.ToString();
+
+        if (telefonostr.Length >9 || telefonostr.Length<9)
+        {
+            ModelState.AddModelError("Telefono", "El número de teléfono es inválido, debe contener 9 dígitos");
+        }
+        else
+        {
+            _context.ActualizarTelefono(numEmpleado, Telefono);
+        }
+
+            // Recargar todos los datos necesarios para la vista
+        EstadisticasUsuario = _context.ObtenerMonedasPorEmpleado(numEmpleado);
+        Usuario = _context.ObtenerUsuarioPorEmpleados(numEmpleado);
+        contacto = _context.ObtenerContactoPorEmpleados(numEmpleado);
+        codigopostal = _context.ObtenerCodigoPorEmpleados(codigoPostal);
+        
+    }
         
 
 }
