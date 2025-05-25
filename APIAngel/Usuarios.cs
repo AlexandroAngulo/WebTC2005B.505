e2336cee8e75312e55508228327493026bdff75b
ChanchayMateo <a00840391@tec.mx>
2025-05-25 14:13:28 -0600

    cambio query consulta usuarios

--- APIAngel/Usuarios.cs
+++ APIAngel/Usuarios.cs
@@ -1,14 +1,16 @@
 namespace APIAngel;
+
 public class Usuarios
 {
-    public int NumEmpleado {get;set;}
-	public string Nombre {get;set;}
-	public string ApellidoP {get;set;}
-	public string ApellidoM {get;set;}
-	public DateTime FechaNacimiento {get;set;}
-	public DateTime FechaRegistro {get;set;}
-	public int IdGenero {get;set;}
-	public int IdTipoPuesto {get;set;}
-	public int IdEstatus {get;set;}
+	public int NumEmpleado { get; set; }
+	public string Nombre { get; set; }
+	public string ApellidoP { get; set; }
+	public string ApellidoM { get; set; }
+	public DateTime FechaNacimiento { get; set; }
+	public DateTime FechaRegistro { get; set; }
+	public int IdGenero { get; set; }
+	public int IdTipoPuesto { get; set; }
+	public int IdEstatus { get; set; }
+	public string Puesto { get; set; }
 
 }
