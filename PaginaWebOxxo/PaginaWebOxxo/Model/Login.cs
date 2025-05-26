b98ad2175fa71bd171d540031c526216d35165fb
Dario <a00838287@tec.mx>
2025-05-26 14:38:52 -0600

    Login

---
+++ PaginaWebOxxo/PaginaWebOxxo/Model/Login.cs
@@ -0,0 +1,7 @@
+using System;
+public class Login
+{
+	public int NumEmpleado { get; set; }
+	public string Contraseña { get; set; }
+	public DateOnly UltimoAcceso { get; set; }
+}
--- PaginaWebOxxo/PaginaWebOxxo/Pages/Index.cshtml.cs
+++ PaginaWebOxxo/PaginaWebOxxo/Pages/Index.cshtml.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PaginaWebOxxo.Model;
 
 namespace PaginaWebOxxo.Pages;
 
 public class IndexModel : PageModel
 {
-
+    private readonly DataBaseContext _context;
+    public IndexModel(DataBaseContext context)
+    {
+        _context = context;
+    }
+    public Login login { get; set; }
     public void OnGet()
     {
 
