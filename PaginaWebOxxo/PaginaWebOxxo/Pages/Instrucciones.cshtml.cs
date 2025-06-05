f85917bb5aab19f443c90eb851bf543e65888218
ChanchayMateo <a00840391@tec.mx>
2025-06-04 18:42:58 -0600

    Instrucciones

---
+++ PaginaWebOxxo/PaginaWebOxxo/Pages/Instrucciones.cshtml.cs
@@ -0,0 +1,11 @@
+using PaginaWebOxxo.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace PaginaWebOxxo.Pages;
+
+public class InstruccionesModel : PageModel
+{
+
+}
